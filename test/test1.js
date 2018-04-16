a57e946c95b654f3417e7fa2f14d7f96b1af0be1
deathcore666 <fr.niez@gmail.com>
2018-04-16 19:07:22 +0600

    - JSON configuration validation - Cassandra connection errors catch

--- test/test1.js
+++ test/test1.js
@@ -1,28 +1,21 @@
-const moment = require('moment');
 const logger = require('../index');
 const logLevels = require('../constants/logLevels');
 const db = require('../cassandra');
 
-const testConfigs = {
-    taskId: 123,
-    component: "M1-1",
-    time: moment().toISOString(),
-    logLevel: logLevels.TRACE,
-};
-
-const msg = {
-    taskId: 12,
-    time: moment().toISOString(),
-    service: 'M1-1',
-    logLevel: logLevels.TRACE,
-    text: 'test'
-};
-
-const dbConf = {
+const configs = {
     contactPoints: ['192.168.0.166'],
     keyspace: 'logs',
     tableName: 'testlogs1',
+    taskId: 0,
+    component: "M1-1",
+    logLevel: logLevels.OFF
 };
 
-logger.init();
+logger.init(configs);
+logger.logFatal('f');
+logger.logError('e');
+logger.logInfo('i');
+logger.logDebug('d');
+logger.logTrace('t');
+
 // logger.logFatal('FATALITY');
