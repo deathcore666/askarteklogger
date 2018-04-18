c572b0e1de09b6bc776bc5b82a7509be188ea4e1
Bakyt <bbalbakov@askartec.com>
2018-04-18 15:39:27 +0600

    fixed es5 import issues fixed tests refactored

--- constants/logLevels.js
+++ constants/logLevels.js
@@ -1,7 +1,17 @@
-export const OFF    = 0;
-export const FATAL  = 1;
-export const ERROR  = 2;
-export const WARN   = 3;
-export const INFO   = 4;
-export const DEBUG  = 5;
-export const TRACE  = 6;
+const OFF    = 0;
+const FATAL  = 1;
+const ERROR  = 2;
+const WARN   = 3;
+const INFO   = 4;
+const DEBUG  = 5;
+const TRACE  = 6;
+
+module.exports = {
+    OFF:    OFF,
+    FATAL:  FATAL,
+    ERROR:  ERROR,
+    WARN:   WARN,
+    INFO:   INFO,
+    DEBUG:  DEBUG,
+    TRACE:  TRACE,
+};
